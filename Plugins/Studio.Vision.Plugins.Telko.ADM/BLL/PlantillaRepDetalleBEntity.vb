4236d1c00b8b02cdfb1db30195ccdd9b8d5a8f32
Administrador <marcelo.blanc@gmail.com>
2022-03-07 13:53:15 -0300

    Se modifican los campos Id de las plantillas de reposicion (cabezal y detalle). Se quitan el autonumerado por base y se pasan a numerar por código.

--- Plugins/Studio.Vision.Plugins.Telko.ADM/BLL/PlantillaRepDetalleBEntity.vb
+++ Plugins/Studio.Vision.Plugins.Telko.ADM/BLL/PlantillaRepDetalleBEntity.vb
@@ -14,6 +14,22 @@ Namespace Business
         Protected Overrides Function CreateEntityFactory() As SD.LLBLGen.Pro.ORMSupportClasses.IEntityFactory2
             Return New Studio.Phone.DAL.FactoryClasses.DV_PlantillaReposicionDetalleEntityFactory
         End Function
+
+        Public Overrides Function SaveEntity(da As IDataAccessAdapter, entity As IEntity2) As Boolean
+            Dim plantila As DV_PlantillaReposicionEntity = DirectCast(entity, DV_PlantillaReposicionEntity)
+            If plantila.Id = 0 Then
+                If da Is Nothing Then
+                    da = CreateAdapter(True)
+                End If
+                plantila.Id = ProximoId(da)
+            End If
+            Return MyBase.SaveEntity(da, entity)
+        End Function
+
+        Private Function ProximoId(da As IDataAccessAdapter) As Integer
+            Dim db As New LinqMetaData(da)
+            Return (From p In db.DV_PlantillaReposicionDetalle Select p.Id).Max() + 1
+        End Function
     End Class
 
 End Namespace
--- Plugins/Studio.Vision.Plugins.Telko.ADM/BLL/PlantillaReposicionBEntity.vb
+++ Plugins/Studio.Vision.Plugins.Telko.ADM/BLL/PlantillaReposicionBEntity.vb
@@ -16,6 +16,24 @@ Namespace Business
             'Return New Studio.Phone.DAL.FactoryClasses.DV_PlantillaReposicionEntityFactory
             Return New Studio.Phone.DAL.FactoryClasses.DV_PlantillaReposicionEntityFactory
         End Function
+
+
+        Public Overrides Function SaveEntity(da As IDataAccessAdapter, entity As IEntity2) As Boolean
+            Dim plantila As DV_PlantillaReposicionEntity = DirectCast(entity, DV_PlantillaReposicionEntity)
+            If plantila.Id = 0 Then
+                If da Is Nothing Then
+                    da = CreateAdapter(True)
+                End If
+                plantila.Id = ProximoId(da)
+            End If
+            Return MyBase.SaveEntity(da, entity)
+        End Function
+
+        Private Function ProximoId(da As IDataAccessAdapter) As Integer
+            Dim db As New LinqMetaData(da)
+            Return (From p In db.DV_PlantillaReposicion Select p.Id).Max() + 1
+        End Function
+
     End Class
 
 End Namespace
